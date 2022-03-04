24221647eb2a4f0aedb48f489bfbf7a2021317df
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-04 23:40:35 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -17,6 +17,166 @@
 		</div>
 	</div>
 
+	<!-- This example requires Tailwind CSS v2.0+ -->
+	<div class="bg-white">
+		<div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+			<div class="max-w-3xl mx-auto text-center">
+				<h2 class="text-3xl font-extrabold text-blue-900 underline decoration-sky-500/30">Мы предлагаем</h2>
+				<p class="mt-4 text-lg text-gray-500">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.</p>
+			</div>
+			<dl class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
+				<div class="relative">
+					<dt>
+						<!-- Heroicon name: outline/check -->
+						<svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+						<p class="ml-9 text-lg leading-6 font-medium text-gray-900">Invite team members</p>
+					</dt>
+					<dd class="mt-2 ml-9 text-base text-gray-500">Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est, molestie blandit quis ac. Lacus.</dd>
+				</div>
+
+				<div class="relative">
+					<dt>
+						<!-- Heroicon name: outline/check -->
+						<svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+						<p class="ml-9 text-lg leading-6 font-medium text-gray-900">Notifications</p>
+					</dt>
+					<dd class="mt-2 ml-9 text-base text-gray-500">Ornare donec rhoncus vitae nisl velit, neque, mauris dictum duis. Nibh urna non parturient.</dd>
+				</div>
+
+				<div class="relative">
+					<dt>
+						<!-- Heroicon name: outline/check -->
+						<svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+						<p class="ml-9 text-lg leading-6 font-medium text-gray-900">List view</p>
+					</dt>
+					<dd class="mt-2 ml-9 text-base text-gray-500">Etiam cras augue ornare pretium sit malesuada morbi orci, venenatis. Dictum lacus.</dd>
+				</div>
+
+				<div class="relative">
+					<dt>
+						<!-- Heroicon name: outline/check -->
+						<svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+						<p class="ml-9 text-lg leading-6 font-medium text-gray-900">Boards</p>
+					</dt>
+					<dd class="mt-2 ml-9 text-base text-gray-500">Interdum quam pulvinar turpis tortor, egestas quis diam amet, natoque. Mauris sagittis.</dd>
+				</div>
+
+				<div class="relative">
+					<dt>
+						<!-- Heroicon name: outline/check -->
+						<svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+						<p class="ml-9 text-lg leading-6 font-medium text-gray-900">Keyboard shortcuts</p>
+					</dt>
+					<dd class="mt-2 ml-9 text-base text-gray-500">Ullamcorper in ipsum ac feugiat. Senectus at aliquam vulputate mollis nec. In at risus odio.</dd>
+				</div>
+
+				<div class="relative">
+					<dt>
+						<!-- Heroicon name: outline/check -->
+						<svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+						<p class="ml-9 text-lg leading-6 font-medium text-gray-900">Reporting</p>
+					</dt>
+					<dd class="mt-2 ml-9 text-base text-gray-500">Magna a vel sagittis aliquam eu amet. Et lorem auctor quam nunc odio. Sed bibendum.</dd>
+				</div>
+
+				<div class="relative">
+					<dt>
+						<!-- Heroicon name: outline/check -->
+						<svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+						<p class="ml-9 text-lg leading-6 font-medium text-gray-900">Calendars</p>
+					</dt>
+					<dd class="mt-2 ml-9 text-base text-gray-500">Sed mi, dapibus turpis orci posuere integer. A porta viverra posuere adipiscing turpis.</dd>
+				</div>
+
+				<div class="relative">
+					<dt>
+						<!-- Heroicon name: outline/check -->
+						<svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+						<p class="ml-9 text-lg leading-6 font-medium text-gray-900">Mobile app</p>
+					</dt>
+					<dd class="mt-2 ml-9 text-base text-gray-500">Quisque sapien nunc nisl eros. Facilisis sagittis maecenas id dignissim tristique proin sed.</dd>
+				</div>
+			</dl>
+		</div>
+	</div>
+
+
+	<!-- This example requires Tailwind CSS v2.0+ -->
+	<div class="bg-gray-50 pt-12 sm:pt-16">
+		<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+			<div class="max-w-4xl mx-auto text-center">
+				<h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">Trusted by developers from over 80 planets</h2>
+				<p class="mt-3 text-xl text-gray-500 sm:mt-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.</p>
+			</div>
+		</div>
+		<div class="mt-10 pb-12 bg-white sm:pb-16">
+			<div class="relative">
+				<div class="absolute inset-0 h-1/2 bg-gray-50"></div>
+				<div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+					<div class="max-w-4xl mx-auto">
+						<dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+							<div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+								<dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Pepperoni</dt>
+								<dd class="order-1 text-5xl font-extrabold text-indigo-600">100%</dd>
+							</div>
+							<div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+								<dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Delivery</dt>
+								<dd class="order-1 text-5xl font-extrabold text-indigo-600">24/7</dd>
+							</div>
+							<div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
+								<dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Calories</dt>
+								<dd class="order-1 text-5xl font-extrabold text-indigo-600">100k</dd>
+							</div>
+						</dl>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- This example requires Tailwind CSS v2.0+ -->
+	<div class="bg-white py-16 lg:py-24">
+		<div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+			<div class="relative py-24 px-8 bg-indigo-500 rounded-xl shadow-2xl overflow-hidden lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8">
+				<div class="absolute inset-0 opacity-50 filter saturate-0 mix-blend-multiply">
+					<img src="https://images.unsplash.com/photo-1601381718415-a05fb0a261f3?ixid=MXwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8ODl8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1216&q=80" alt="" class="w-full h-full object-cover">
+				</div>
+				<div class="relative lg:col-span-1">
+					<img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workcation-logo-white.svg" alt="">
+					<blockquote class="mt-6 text-white">
+						<p class="text-xl font-medium sm:text-2xl">Workflow has completely transformed how we interact with customers. We've seen record bookings, higher customer satisfaction, and reduced churn.</p>
+						<footer class="mt-6">
+							<p class="flex flex-col font-medium">
+								<span>Marie Chilvers</span>
+								<span>CEO, Workcation</span>
+							</p>
+						</footer>
+					</blockquote>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
 </section>
 
 
