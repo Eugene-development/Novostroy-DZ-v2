3ca208a4558c22c62210da30a680f3ccba6c29b9
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-05 16:45:22 +0300

    git init

--- app/src/lib/form.js
+++ app/src/lib/form.js
@@ -1,7 +1,7 @@
 import { invalidate } from '$app/navigation';
 
 // this action (https://svelte.dev/tutorial/actions) allows us to
-// progressively enhance a <form> that already works without JS
+// progressively enhance a <form> that already work without JS
 export function enhance(form, { pending, error, result } = {}) {
 	let current_token;
 
