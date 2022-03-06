d8bf3e6241070a8f1b02ee3644b6321fc14bb569
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-06 19:42:00 +0300

    git init

--- app/src/lib/header/index.svelte
+++ app/src/lib/header/index.svelte
@@ -1,11 +1,11 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="relative bg-white">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
-        <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+        <div class="flex justify-between items-center border-b-2 border-gray-100 py-2 md:justify-start md:space-x-10">
             <div class="flex justify-start lg:w-0 lg:flex-1">
-                <a href="#">
-                    <span class="sr-only">Workflow</span>
-                    <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
+                <a href="/">
+                    <span class="sr-only">Logo</span>
+                    <img class="h-10 w-auto sm:h-10" src="static/logo-header.png" alt="">
                 </a>
             </div>
             <div class="-mr-2 -my-2 md:hidden">
--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -2,14 +2,16 @@
 	<title>Ремонт квартир в Дзержинске</title>
 </svelte:head>
 
+
+
 <section>
 
 	<!-- This example requires Tailwind CSS v2.0+ -->
 	<div class="bg-gray-50 pt-12 sm:pt-16">
 		<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 			<div class="max-w-4xl mx-auto text-center">
-				<h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">Строительно-ремонтная компания "Новострой"</h2>
-				<p class="mt-3 text-xl text-gray-500 sm:mt-4">Высококачественный профессиональный ремонт помещений. Строительство малоэтажных конструкций, бань, дач и технических сооружений.</p>
+				<h2 class="text-3xl font-extrabold text-cyan-900 sm:text-5xl">Ремонтно-строительная компания "Новострой"</h2>
+				<p class="mt-3 text-xl text-gray-500 sm:mt-4">Профессиональный полный или частичный ремонт квартир в Дзержинске. Строительство малоэтажных конструкций, бань, дач и технических сооружений.</p>
 			</div>
 		</div>
 		<div class="mt-10 pb-12 bg-white sm:pb-16">
@@ -20,15 +22,15 @@
 						<dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
 							<div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
 								<dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">гарантии</dt>
-								<dd class="order-1 text-5xl font-extrabold text-indigo-600">100%</dd>
+								<dd class="order-1 text-5xl font-extrabold text-cyan-600">100%</dd>
 							</div>
 							<div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
 								<dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">поддержка</dt>
-								<dd class="order-1 text-5xl font-extrabold text-indigo-600">24/7</dd>
+								<dd class="order-1 text-5xl font-extrabold text-cyan-600">24/7</dd>
 							</div>
 							<div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
 								<dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">объекта</dt>
-								<dd class="order-1 text-5xl font-extrabold text-indigo-600">483</dd>
+								<dd class="order-1 text-5xl font-extrabold text-cyan-600">483</dd>
 							</div>
 						</dl>
 					</div>
@@ -37,49 +39,110 @@
 		</div>
 	</div>
 
+	<figure>
+		<img class="w-full px-48" src="static/logo.png" alt="">
+	</figure>
 
 	<!-- This example requires Tailwind CSS v2.0+ -->
-	<div class="bg-white py-16 lg:py-24">
-		<div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-			<div class="relative py-24 px-8 bg-indigo-500 rounded-xl shadow-2xl overflow-hidden lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8">
-				<div class="absolute inset-0 opacity-50 filter saturate-0 mix-blend-multiply">
-					<img src="https://images.unsplash.com/photo-1601381718415-a05fb0a261f3?ixid=MXwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8ODl8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1216&q=80" alt="" class="w-full h-full object-cover">
-				</div>
-				<div class="relative lg:col-span-1">
-					<img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workcation-logo-white.svg" alt="">
-					<blockquote class="mt-6 text-white">
-						<p class="text-xl font-medium sm:text-2xl">Быстро и качественно выполняем работы по ремонту помещений и возведению сооружений различного назначения. Работаем с физическими и юридическими лицами. Будем рады сотрудничеству.</p>
-						<footer class="mt-6">
-							<p class="flex flex-col font-medium">
-								<span>Главный директор</span>
-								<span>Попугаева Алёна Евгеньевна &#128105;</span>
+<!--	<div class="relative bg-gray-50">-->
+
+<!--		<main class="lg:relative">-->
+<!--			<div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">-->
+<!--				<div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">-->
+<!--					<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">-->
+<!--						<span class="block xl:inline">Data to enrich your</span>-->
+<!--						<span class="block text-indigo-600 xl:inline">online business</span>-->
+<!--					</h1>-->
+<!--					<p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>-->
+<!--					<div class="mt-10 sm:flex sm:justify-center lg:justify-start">-->
+<!--						<div class="rounded-md shadow">-->
+<!--							<a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Get started </a>-->
+<!--						</div>-->
+<!--						<div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">-->
+<!--							<a href="#" class="w-5xl flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"> Live demo </a>-->
+<!--						</div>-->
+<!--					</div>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--			<div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">-->
+<!--				<img class="absolute inset-0 w-full h-full object-cover" src="static/logo.png" alt="logo">-->
+<!--			</div>-->
+<!--		</main>-->
+<!--	</div>-->
+
+	<div class="">
+		<div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+<!--			<div class="sm:flex sm:items-baseline sm:justify-between">-->
+<!--				<h2 class="text-2xl font-extrabold tracking-tight text-gray-900">Стиль ремонта</h2>-->
+<!--			</div>-->
+
+			<div class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+				<div class="group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2">
+					<img src="https://tailwindui.com/img/ecommerce-images/home-page-03-featured-category.jpg" alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee." class="object-center object-cover group-hover:opacity-75">
+					<div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50"></div>
+					<div class="p-6 flex items-end">
+						<div>
+							<h3 class="font-semibold text-white">
+								<a href="#">
+									<span class="absolute inset-0"></span>
+									New Arrivals
+								</a>
+							</h3>
+							<p aria-hidden="true" class="mt-1 text-sm text-white">
+								Shop now
 							</p>
-						</footer>
-					</blockquote>
+						</div>
+					</div>
 				</div>
+				<div class="group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:relative sm:aspect-none sm:h-full">
+					<img src="https://tailwindui.com/img/ecommerce-images/home-page-03-category-01.jpg" alt="Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters." class="object-center object-cover group-hover:opacity-75 sm:absolute sm:inset-0 sm:w-full sm:h-full">
+					<div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
+					<div class="p-6 flex items-end sm:absolute sm:inset-0">
+						<div>
+							<h3 class="font-semibold text-white">
+								<a href="#">
+									<span class="absolute inset-0"></span>
+									Accessories
+								</a>
+							</h3>
+							<p aria-hidden="true" class="mt-1 text-sm text-white">
+								Shop now
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:relative sm:aspect-none sm:h-full">
+					<img src="https://tailwindui.com/img/ecommerce-images/home-page-03-category-02.jpg" alt="Walnut desk organizer set with white modular trays, next to porcelain mug on wooden desk." class="object-center object-cover group-hover:opacity-75 sm:absolute sm:inset-0 sm:w-full sm:h-full">
+					<div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
+					<div class="p-6 flex items-end sm:absolute sm:inset-0">
+						<div>
+							<h3 class="font-semibold text-white">
+								<a href="#">
+									<span class="absolute inset-0"></span>
+									Workspace
+								</a>
+							</h3>
+							<p aria-hidden="true" class="mt-1 text-sm text-white">
+								Shop now
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="mt-6 sm:hidden">
+				<a href="#" class="block text-sm font-semibold text-indigo-600 hover:text-indigo-500">Browse all categories<span aria-hidden="true"> &rarr;</span></a>
 			</div>
 		</div>
 	</div>
 
-	<!-- This example requires Tailwind CSS v2.0+ -->
-	<div class="bg-blue-700">
-		<div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-			<h2 class="text-3xl font-extrabold text-white sm:text-4xl">
-				<span class="block">Ремонт квартир в Дзержинске</span>
-				<span class="block">Нижегородской области</span>
-<!--				<img alt="logo" class="h-32" src="https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/logo/logo.png">-->
 
-			</h2>
-			<p class="mt-4 text-lg leading-6 text-indigo-200">Быстро и качественно выполняем работы по ремонту помещений. Работаем с физическими и юридическими лицами.</p>
-			<a href="/" class="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50 sm:w-auto"> Подробнее </a>
-		</div>
-	</div>
 
 	<!-- This example requires Tailwind CSS v2.0+ -->
 	<div class="bg-white">
 		<div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
 			<div class="max-w-3xl mx-auto text-center">
-				<h2 class="text-3xl font-extrabold text-blue-900 underline decoration-sky-500/30">Мы предлагаем</h2>
+				<h2 class="text-3xl font-extrabold text-gray-700">Мы предлагаем</h2>
 				<p class="mt-4 text-lg text-gray-500">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.</p>
 			</div>
 			<dl class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
@@ -174,7 +237,50 @@
 		</div>
 	</div>
 
+	<!-- This example requires Tailwind CSS v2.0+ -->
+	<div class="bg-white py-16 lg:py-24">
+		<div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+			<div class="relative py-24 px-8 bg-cyan-800 rounded-xl shadow-2xl overflow-hidden lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8">
+				<div class="absolute inset-0 opacity-50 filter saturate-0 mix-blend-multiply">
+					<img src="https://images.unsplash.com/photo-1601381718415-a05fb0a261f3?ixid=MXwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8ODl8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1216&q=80" alt="" class="w-full h-full object-cover">
+				</div>
+				<div class="relative lg:col-span-1">
+					<img class="h-12 w-auto" src="static/logo-header.png" alt="">
+					<blockquote class="mt-6 text-white">
+						<p class="text-xl font-medium sm:text-2xl">Быстро и качественно выполняем работы по ремонту помещений и возведению сооружений различного назначения. Работаем с физическими и юридическими лицами. Будем рады сотрудничеству.</p>
+						<footer class="mt-6">
+							<p class="flex flex-col font-medium">
+								<span>Главный директор</span>
+								<span>Попугаева Алёна Евгеньевна &#128105;</span>
+							</p>
+						</footer>
+					</blockquote>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 
 </section>
 
 
+
+<div class="bg-white">
+	<div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl">
+		<div class="relative rounded-lg overflow-hidden lg:h-96">
+			<div class="absolute inset-0">
+				<img src="https://tailwindui.com/img/ecommerce-images/category-page-01-featured-collection.jpg" alt="" class="w-full h-full object-center object-cover">
+			</div>
+			<div aria-hidden="true" class="relative w-full h-96 lg:hidden"></div>
+			<div aria-hidden="true" class="relative w-full h-32 lg:hidden"></div>
+			<div class="absolute inset-x-0 bottom-0 bg-black bg-opacity-75 p-6 rounded-bl-lg rounded-br-lg backdrop-filter backdrop-blur sm:flex sm:items-center sm:justify-between lg:inset-y-0 lg:inset-x-auto lg:w-96 lg:rounded-tl-lg lg:rounded-br-none lg:flex-col lg:items-start">
+				<div>
+					<h2 class="text-xl font-bold text-white">Workspace Collection</h2>
+					<p class="mt-1 text-sm text-gray-300">Upgrade your desk with objects that keep you organized and clear-minded.</p>
+				</div>
+				<a href="#" class="mt-6 flex-shrink-0 flex bg-white bg-opacity-0 py-3 px-4 border border-white border-opacity-25 rounded-md items-center justify-center text-base font-medium text-white hover:bg-opacity-10 sm:mt-0 sm:ml-8 lg:ml-0 lg:w-full">View the collection</a>
+			</div>
+		</div>
+	</div>
+</div>
