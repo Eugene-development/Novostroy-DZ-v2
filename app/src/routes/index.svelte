af9c785a95d966b976de0de505bcf09c8fa168d6
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-19 21:10:36 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -6,6 +6,94 @@
 
 <section>
 
+	<!--
+  This example requires Tailwind CSS v2.0+
+
+  This example requires some changes to your config:
+
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/typography'),
+    ],
+  }
+  ```
+-->
+	<div class="py-16 bg-gray-50 overflow-hidden">
+		<div class="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+			<div class="text-base max-w-prose mx-auto lg:max-w-none">
+				<h2 class="text-base text-purple-600 font-semibold tracking-wide uppercase">Transactions</h2>
+				<p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Ремонт квартир любой сложности на лучших условиях</p>
+			</div>
+			<div class="relative z-10 text-base max-w-prose mx-auto lg:max-w-5xl lg:mx-0 lg:pr-72">
+				<p class="py-1 text-lg text-gray-500">Время от времени у каждого возникает необходимость облагородить свое родное помещение. Если и для вас настал такой момент, доверьте эту работу настоящим профессионалам. </p>
+				<p class="py-1 text-lg text-gray-500">Опытные квалифицированные сотрудники компании «Novostroy» выполнят работы любой сложности. Для нас совершенно нет разницы, кухня ли это, гостиная, ванная, спальня, или же вся квартира целиком.  </p>
+				<p class="py-1 text-lg text-gray-500">Качественный ремонт квартир осуществляется, в первую очередь, чтобы вы могли жить в максимально комфортных условиях.   </p>
+			</div>
+			<div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-start">
+				<div class="relative z-10">
+					<div class="prose prose-indigo text-gray-500 mx-auto lg:max-w-none">
+						<p class="text-lg">Для этого наши специалисты:</p>
+						<ul role="list">
+							<li>полностью демонтируют отделку</li>
+							<li>сносят и устанавливают стены, а также перегородки, используя самые качественные материалы</li>
+							<li>выравнивают потолки и стены</li>
+							<li>осуществляют прокладку звукоизоляции</li>
+							<li>окрашивают и грунтуют поверхность</li>
+							<li>монтируют окна и двери</li>
+							<li>самостоятельно устанавливают мебель по ранее согласованному проекту, а также выполняют все остальные работы</li>
+						</ul>
+						<p>Мы также тщательно следим за каждым сотрудником. К нам просто не может попасть непрофессионал или человек, который относится к своей работе легкомысленно. </p>
+						<h3>Мы готовы вам помочь</h3>
+						<p>Запишитесь на бесплатную консультацию или задайте ваш вопрос в чате. Мы всегда будем рады общению с вами и ответим на любые вопросы</p>
+					</div>
+					<div class="mt-10 flex text-base max-w-prose mx-auto lg:max-w-none">
+						<div class="rounded-md shadow">
+							<a href="#" class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Contact sales </a>
+						</div>
+						<div class="rounded-md shadow ml-4">
+							<a href="#" class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"> Learn more </a>
+						</div>
+					</div>
+				</div>
+				<div class="mt-12 relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none">
+					<svg class="absolute top-0 right-0 -mt-20 -mr-20 lg:top-auto lg:right-auto lg:bottom-1/2 lg:left-1/2 lg:mt-0 lg:mr-0 xl:top-0 xl:right-0 xl:-mt-20 xl:-mr-20" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
+						<defs>
+							<pattern id="bedc54bc-7371-44a2-a2bc-dc68d819ae60" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+								<rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+							</pattern>
+						</defs>
+						<rect width="404" height="384" fill="url(#bedc54bc-7371-44a2-a2bc-dc68d819ae60)" />
+					</svg>
+					<blockquote class="relative bg-white rounded-lg shadow-lg">
+						<div class="rounded-t-lg px-6 py-8 sm:px-10 sm:pt-10 sm:pb-8">
+							<img src="static/logo-header.png" alt="logo" class="h-16">
+							<div class="relative text-lg text-gray-700 font-medium mt-8">
+								<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-gray-200" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+									<path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+								</svg>
+								<p class="relative">Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam. Montes, magna cursus nulla feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu proin massa, lectus.</p>
+							</div>
+						</div>
+						<cite class="relative flex items-center sm:items-start bg-purple-600 rounded-b-lg not-italic py-5 px-6 sm:py-5 sm:pl-12 sm:pr-10 sm:mt-10">
+							<div class="relative rounded-full border-2 border-white sm:absolute sm:top-0 sm:transform sm:-translate-y-1/2">
+								<img class="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-purple-300" src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.5&w=160&h=160&q=80" alt="">
+							</div>
+							<span class="relative ml-4 text-indigo-300 font-semibold leading-6 sm:ml-24 sm:pl-1">
+							  <p class="text-white font-semibold sm:inline">Judith Black</p>
+							  <p class="sm:inline">CEO at Workcation</p>
+							</span>
+						</cite>
+					</blockquote>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 
 
 	<!--
@@ -111,7 +199,7 @@
 			<div class="max-w-2xl mx-auto px-4 lg:max-w-none">
 				<div class="max-w-3xl">
 					<h2 class="font-semibold text-gray-500">Стилистика</h2>
-					<p class="mt-2 text-3xl font-extrabold tracking-tight text-cyan-700 sm:text-5xl">Выбирайте то, что вам нравится</p>
+					<p class="mt-2 text-3xl font-extrabold tracking-tight text-cyan-700 sm:text-5xl">Выбирайте стиль по душе</p>
 					<p class="mt-4 text-gray-500">The Drawstring Canister comes with multiple strap and handle options to adapt throughout your day. Shoulder sling it, backpack it, or handy carry it.</p>
 				</div>
 
