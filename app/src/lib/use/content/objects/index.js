bbe87ae96109b187dbabf539611584abdf63917a
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-30 22:25:28 +0300

    git init

---
+++ app/src/lib/use/content/objects/index.js
@@ -0,0 +1,12 @@
+const Objects = [
+    {
+        id: 1,
+        title: 'Двухкомнатная квартира',
+        address: 'п-т Ленина, 84',
+        img: 'FullSizeRender.jpg'
+    }
+]
+
+export const useObjects = {
+    Objects
+}
--- app/src/routes/objects/index.svelte
+++ app/src/routes/objects/index.svelte
@@ -1,3 +1,7 @@
+<script>
+    import {useObjects} from "$lib/use/content/objects";
+    const {Objects} = useObjects
+</script>
 
 <div class="bg-gradient-to-b from-white to-gray-50">
     <div class="max-w-7xl mx-auto px-4 py-16 sm:px-6 lg:px-8">
@@ -11,44 +15,22 @@
 
         <div class="max-w-2xl mx-auto py-8 sm:py-16 lg:py-24 lg:max-w-none">
             <div class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
-                <div class="group relative">
-                    <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-                        <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="w-full h-full object-center object-cover">
-                    </div>
-                    <h3 class="mt-6 text-sm text-gray-500">
-                        <a href="#">
-                            <span class="absolute inset-0"></span>
-                            Desk and Office
-                        </a>
-                    </h3>
-                    <p class="text-base font-semibold text-gray-900">Work from home accessories</p>
-                </div>
 
-                <div class="group relative">
-                    <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-                        <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="w-full h-full object-center object-cover">
+                {#each Objects as {title, address, img}}
+                    <div class="group relative">
+                        <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+                            <img src="objects/{img}" alt="object" class="w-full h-full object-center object-cover">
+                        </div>
+                        <h3 class="mt-6 text-sm text-gray-500">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                {title}
+                            </a>
+                        </h3>
+                        <p class="text-base font-semibold text-gray-900">{address}</p>
                     </div>
-                    <h3 class="mt-6 text-sm text-gray-500">
-                        <a href="#">
-                            <span class="absolute inset-0"></span>
-                            Self-Improvement
-                        </a>
-                    </h3>
-                    <p class="text-base font-semibold text-gray-900">Journals and note-taking</p>
-                </div>
+                {/each}
 
-                <div class="group relative">
-                    <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-                        <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="w-full h-full object-center object-cover">
-                    </div>
-                    <h3 class="mt-6 text-sm text-gray-500">
-                        <a href="#">
-                            <span class="absolute inset-0"></span>
-                            Travel
-                        </a>
-                    </h3>
-                    <p class="text-base font-semibold text-gray-900">Daily commute essentials</p>
-                </div>
             </div>
         </div>
     </div>
