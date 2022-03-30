cc20c278544ddb80230406bbedbb8083b43d5d63
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-30 21:49:32 +0300

    git init

--- app/src/routes/blog/index.svelte
+++ app/src/routes/blog/index.svelte
@@ -10,16 +10,16 @@
     <div class="relative py-16 max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
 
         <div>
-            <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Тематические статьи</h2>
+            <h2 class="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl">Всё о ремонте квартир</h2>
             <div class="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
-                <p class="text-xl text-gray-500">Get weekly articles in your inbox on how to grow your business.</p>
+                <p class="text-xl text-gray-500">Интересные статьи с полезной информацией и советами.</p>
                 <form class="mt-6 flex flex-col sm:flex-row lg:mt-0 lg:justify-end">
                     <div>
                         <label for="email-address" class="sr-only">Email address</label>
-                        <input id="email-address" name="email-address" type="email" autocomplete="email" required class="appearance-none w-full px-4 py-2 border border-gray-300 text-base rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:ring-sky-500 focus:border-sky-500 lg:max-w-xs" placeholder="Enter your email">
+                        <input id="email-address" name="email-address" type="email" autocomplete="email" required class="appearance-none w-full px-4 py-2 border border-gray-300 text-base rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:ring-sky-500 focus:border-sky-500 lg:max-w-xs" placeholder="Введите почту">
                     </div>
                     <div class="mt-2 flex-shrink-0 w-full flex rounded-md shadow-sm sm:mt-0 sm:ml-3 sm:w-auto sm:inline-flex">
-                        <button type="button" class="w-full bg-sky-600 px-4 py-2 border border-transparent rounded-md flex items-center justify-center text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:w-auto sm:inline-flex">Notify me</button>
+                        <button type="button" class="w-full bg-sky-600 px-4 py-2 border border-transparent rounded-md flex items-center justify-center text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:w-auto sm:inline-flex">Подписаться</button>
                     </div>
                 </form>
             </div>
@@ -27,55 +27,17 @@
         <div class="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
             <div>
                 <p class="text-sm text-gray-500">
-                    <time datetime="2020-03-16">Mar 16, 2020</time>
+                    <time datetime="2020-03-16">20 марта, 2022</time>
                 </p>
                 <a href="#" class="mt-2 block">
                     <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
                     <p class="mt-3 text-base text-gray-500">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
                 </a>
                 <div class="mt-3">
-                    <a href="#" class="text-base font-semibold text-sky-600 hover:text-sky-500"> Read full story </a>
+                    <a href="#" class="text-base font-semibold text-sky-600 hover:text-sky-500"> Читать полностью </a>
                 </div>
             </div>
 
-            <div>
-                <p class="text-sm text-gray-500">
-                    <time datetime="2020-03-10">Mar 10, 2020</time>
-                </p>
-                <a href="#" class="mt-2 block">
-                    <p class="text-xl font-semibold text-gray-900">How to use search engine optimization to drive sales</p>
-                    <p class="mt-3 text-base text-gray-500">Optio cum necessitatibus dolor voluptatum provident commodi et. Qui aperiam fugiat nemo cumque.</p>
-                </a>
-                <div class="mt-3">
-                    <a href="#" class="text-base font-semibold text-sky-600 hover:text-sky-500"> Read full story </a>
-                </div>
-            </div>
-
-            <div>
-                <p class="text-sm text-gray-500">
-                    <time datetime="2020-02-12">Feb 12, 2020</time>
-                </p>
-                <a href="#" class="mt-2 block">
-                    <p class="text-xl font-semibold text-gray-900">Improve your customer experience</p>
-                    <p class="mt-3 text-base text-gray-500">Cupiditate maiores ullam eveniet adipisci in doloribus nulla minus. Voluptas iusto libero adipisci rem et corporis.</p>
-                </a>
-                <div class="mt-3">
-                    <a href="#" class="text-base font-semibold text-sky-600 hover:text-sky-500"> Read full story </a>
-                </div>
-            </div>
-
-            <div>
-                <p class="text-sm text-gray-500">
-                    <time datetime="2020-01-29">Jan 29, 2020</time>
-                </p>
-                <a href="#" class="mt-2 block">
-                    <p class="text-xl font-semibold text-gray-900">Writing effective landing page copy</p>
-                    <p class="mt-3 text-base text-gray-500">Ipsum voluptates quia doloremque culpa qui eius. Id qui id officia molestias quaerat deleniti. Qui facere numquam autem libero quae cupiditate asperiores vitae cupiditate. Cumque id deleniti explicabo.</p>
-                </a>
-                <div class="mt-3">
-                    <a href="#" class="text-base font-semibold text-sky-600 hover:text-sky-500"> Read full story </a>
-                </div>
-            </div>
         </div>
     </div>
 </div>
