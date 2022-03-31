3b468221ae7e2f03a1eae32fb547ce5b1db0d18a
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-31 17:11:10 +0300

    git init

--- app/src/routes/contacts/index.svelte
+++ app/src/routes/contacts/index.svelte
@@ -38,6 +38,16 @@
                             <p>novobuild@yandex.ru</p>
                         </div>
                     </div>
+                    <div class="mt-6 flex">
+                        <div class="flex-shrink-0">
+                            <!-- Heroicon name: outline/mail -->
+                            <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>                        </div>
+                        <div class="ml-3 text-base text-gray-500">
+                            <a href="https://t.me/NovoStrouy">Телеграм: @NovoStrouy</a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="mt-12 sm:mt-16 md:mt-0">
