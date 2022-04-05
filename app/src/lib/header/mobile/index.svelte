23df8c9372bcef00c3f2b833749f04e8fc39bd92
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-05 16:56:06 +0300

    git init

--- app/src/lib/header/mobile/index.svelte
+++ app/src/lib/header/mobile/index.svelte
@@ -28,17 +28,19 @@
     To: "opacity-0 scale-95"
 -->
 
+{#if (visibleMobileMenu)}
+
 
     <div class="absolute z-20 top-0 inset-x-0 p-2 transition transform origin-top lg:hidden bg-gray-50">
         <div class="rounded-lg shadow-md  ring-1 ring-black ring-opacity-5 overflow-hidden">
             <div class="px-5 pt-4 flex items-center justify-between">
                 <a on:click={changeVisibleMobileMenu} href="/">
-                    <img class="h-8 w-auto" src="/logo-header.png" alt="main">
+                    <img class="h-12 w-auto" src="/logo-header.png" alt="main">
                 </a>
 
                 <div class="-mr-2">
                     <button on:click={changeVisibleMobileMenu} type="button"
-                            class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                            class="bg-sky-700 rounded-md p-2 inline-flex items-center justify-center text-gray-100 hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
                         <span class="sr-only">Close menu</span>
                         <!-- Heroicon name: outline/x -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -51,7 +53,7 @@
             <div class="pt-5 pb-2">
                 <div class="px-2 space-y-1">
                     {#each Menu as { value, link }, i}
-                        <a class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50" href="/{link}" on:click={changeVisibleMobileMenu}>{value}</a>
+                        <a class="block px-3 py-2 rounded-md text-base text-center font-medium text-gray-700 hover:text-gray-100 hover:bg-sky-500" href="/{link}" on:click={changeVisibleMobileMenu}>{value}</a>
                     {:else}
                         <p>Нет данных!</p>
                     {/each}
@@ -66,3 +68,4 @@
             </div>
         </div>
     </div>
+    {/if}
