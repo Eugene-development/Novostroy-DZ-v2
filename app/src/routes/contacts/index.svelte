208894f62821a1c67b069157bede25c09d87cb01
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-06 12:40:03 +0300

    git init

--- app/src/routes/contacts/index.svelte
+++ app/src/routes/contacts/index.svelte
@@ -19,34 +19,34 @@
 
 
         <!-- This example requires Tailwind CSS v2.0+ -->
-        <section class="py-12 overflow-hidden md:py-16 lg:py-12">
-            <div class="relative max-w-full mx-auto px-4 sm:px-6 lg:px-8">
-                <div class="relative">
-                    <img class="mx-auto h-96" src="/logo.png" alt="logo">
-                    <blockquote class="">
-                        <div class="mt-8 max-w-3xl mx-auto text-center text-2xl leading-9 font-medium text-gray-900">
-                            <p>&ldquo;Благодаря огромному опыту, а также использованию самого современного оборудования и инструментов, ремонт будет выполнен не только качественно, но и в самые короткие сроки.&rdquo;</p>
-                        </div>
-                        <footer class="mt-8">
-                            <div class="md:flex md:items-center md:justify-center">
-                                <div class="md:flex-shrink-0">
-                                    <img class="mx-auto h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                                </div>
-                                <div class="mt-3 text-center md:mt-0 md:ml-4 md:flex md:items-center">
-                                    <div class="text-base font-medium text-gray-900">Иванов Иван</div>
+<!--        <section class="py-12 overflow-hidden md:py-16 lg:py-12">-->
+<!--            <div class="relative max-w-full mx-auto px-4 sm:px-6 lg:px-8">-->
+<!--                <div class="relative">-->
+<!--                    <img class="mx-auto h-96" src="/logo.png" alt="logo">-->
+<!--                    <blockquote class="">-->
+<!--                        <div class="mt-8 max-w-3xl mx-auto text-center text-2xl leading-9 font-medium text-gray-900">-->
+<!--                            <p>&ldquo;Благодаря огромному опыту, а также использованию самого современного оборудования и инструментов, ремонт будет выполнен не только качественно, но и в самые короткие сроки.&rdquo;</p>-->
+<!--                        </div>-->
+<!--                        <footer class="mt-8">-->
+<!--                            <div class="md:flex md:items-center md:justify-center">-->
+<!--                                <div class="md:flex-shrink-0">-->
+<!--                                    <img class="mx-auto h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">-->
+<!--                                </div>-->
+<!--                                <div class="mt-3 text-center md:mt-0 md:ml-4 md:flex md:items-center">-->
+<!--                                    <div class="text-base font-medium text-gray-900">Иванов Иван</div>-->
 
-                                    <svg class="hidden md:block mx-1 h-5 w-5 text-sky-600" fill="currentColor" viewBox="0 0 20 20">
-                                        <path d="M11 0h3L9 20H6l5-20z" />
-                                    </svg>
+<!--                                    <svg class="hidden md:block mx-1 h-5 w-5 text-sky-600" fill="currentColor" viewBox="0 0 20 20">-->
+<!--                                        <path d="M11 0h3L9 20H6l5-20z" />-->
+<!--                                    </svg>-->
 
-                                    <div class="text-base font-medium text-gray-500">Генеральный директор</div>
-                                </div>
-                            </div>
-                        </footer>
-                    </blockquote>
-                </div>
-            </div>
-        </section>
+<!--                                    <div class="text-base font-medium text-gray-500">Генеральный директор</div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                        </footer>-->
+<!--                    </blockquote>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </section>-->
 
 
         <div class="max-w-lg py-16 mx-auto md:max-w-none md:grid md:grid-cols-2 md:gap-8">
