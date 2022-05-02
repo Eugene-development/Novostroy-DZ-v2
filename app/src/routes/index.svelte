42591d3d25c24e7034950f4a3c240d8524a59bf1
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-02 21:13:25 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -154,10 +154,10 @@
 				<div class="relative">
 					<dt>
 						<!-- Heroicon name: outline/check -->
-						<svg class="absolute h-6 w-6 text-red-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+						<svg class="absolute h-6 w-6 text-cyan-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
 						</svg>
-						<p class="ml-9 text-lg leading-6 font-medium text-red-900">{title}</p>
+						<p class="ml-9 text-lg leading-6 font-medium text-cyan-900">{title}</p>
 					</dt>
 					<dd class="mt-2 ml-9 text-base text-gray-500">{text}</dd>
 				</div>
