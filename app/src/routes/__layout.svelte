4277def8db9343836a2938cc4395c3740c4a12a0
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-24 13:23:15 +0300

    с

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -1,14 +1,15 @@
+
+
 <script>
-	import Header from '$lib/header/index.svelte';
-	import Footer from '$lib/footer/index.svelte'
-	import '../app.css';
+  import Header from "$lib/header/index.svelte";
+  import Footer from "$lib/footer/index.svelte";
+  import "../app.css";
 </script>
 
 <Header />
 
 <main>
-	<slot />
+  <slot />
 </main>
 
 <Footer />
-
